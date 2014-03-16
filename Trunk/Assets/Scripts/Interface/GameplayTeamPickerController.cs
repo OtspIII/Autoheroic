a0d3e7fe66bd398f0f12df3fba2570d90f6f6a06
Misha Favorov <otspiii@gmail.com>
2014-03-16 16:46:16 -0400

    New combat screen team pickers working. Now to just make the game play once you select them both.

--- Trunk/Assets/Scripts/Interface/GameplayTeamPickerController.cs
+++ Trunk/Assets/Scripts/Interface/GameplayTeamPickerController.cs
@@ -8,6 +8,13 @@ public class GameplayTeamPickerController : MonoBehaviour {
     List<Cub.Tool.Team> Teams;
     List<UIPopupList> Selectors = new List<UIPopupList>();
     Dictionary<string, Team> TeamDictionary = new Dictionary<string, Team>();
+    UIPopupList SelOne;
+    UIPopupList SelTwo;
+    UIButton ReadyOne;
+    UIButton ReadyTwo;
+    bool T1Ready = false;
+    bool T2Ready = false;
+    bool GameStarted = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +23,23 @@ public class GameplayTeamPickerController : MonoBehaviour {
         {
             switch (child.gameObject.name)
             {
-                case "Team Picker":
-                    Selectors.Add((UIPopupList)child.gameObject.GetComponentInChildren(System.Type.GetType("UIPopupList")));
+                case "Team Picker One":
+                    SelOne = (UIPopupList)child.gameObject.GetComponentInChildren(System.Type.GetType("UIPopupList"));
+                    Selectors.Add(SelOne);
+                    break;
+                case "Team Picker Two":
+                    SelTwo = (UIPopupList)child.gameObject.GetComponentInChildren(System.Type.GetType("UIPopupList"));
+                    Selectors.Add(SelTwo);
+                    break;
+                case "Team One Ready":
+                    ReadyOne = (UIButton)child.gameObject.GetComponent("UIButton");
+                    break;
+                case "Team Two Ready":
+                    ReadyTwo = (UIButton)child.gameObject.GetComponent("UIButton");
                     break;
             }
         }
-        List<string> teamNames = new List<string>();
+        List<string> teamNames = new List<string> { "-Select a Team-" };
         foreach (Team t in Teams)
         {
             string name = t.Name;
@@ -33,21 +51,61 @@ public class GameplayTeamPickerController : MonoBehaviour {
         foreach (UIPopupList pop in Selectors)
         {
             pop.items = teamNames;
-            if (teamNames.Count > 0)
-                pop.value = teamNames[0];
-            else
-                pop.value = "No Teams Yet Created";
+            pop.value = teamNames[0];
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (GameStarted) return;
+        if (T1Ready && SelOne.value == SelOne.items[0])
+            TeamOneButton();
+        if (T2Ready && SelTwo.value == SelTwo.items[0])
+            TeamTwoButton();
+        if (T1Ready && T2Ready)
+            GameStart();
+            
 	}
 
     List<Team> LoadTeams()
     {
         string name = typeof(List<Team>).AssemblyQualifiedName;
         return (List<Team>)Cub.Tool.Xml.Deserialize(System.Type.GetType(name), "Data/TeamSaves.xml");
+    }
+
+    public void TeamOneButton()
+    {
+        if (T1Ready)
+        {
+            T1Ready = false;
+            ReadyOne.defaultColor = Color.white;
+        }
+        else if (SelOne.value != SelOne.items[0])
+        { 
+            T1Ready = true;
+            ReadyOne.defaultColor = Color.green;
+        }
+        ((UISprite)ReadyOne.GetComponent("UISprite")).color = ReadyOne.defaultColor;
+    }
+
+    public void TeamTwoButton()
+    {
+        if (T2Ready)
+        {
+            T2Ready = false;
+            ReadyTwo.defaultColor = Color.white;
+        }
+        else if (SelTwo.value != SelTwo.items[0])
+        {
+            T2Ready = true;
+            ReadyTwo.defaultColor = Color.green;
+        }
+        ((UISprite)ReadyTwo.GetComponent("UISprite")).color = ReadyTwo.defaultColor;
+    }
+
+    void GameStart()
+    {
+        GameStarted = true;
+        Debug.Log("Game Starts");
     }
 }
