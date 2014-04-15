edc304d65dc2521c37486d01179d0b03a085455a
Misha Favorov <otspiii@gmail.com>
2014-04-15 03:12:05 -0400

    Cleaning up merge issues.

--- Trunk/Assets/Scripts/View/Part.cs
+++ Trunk/Assets/Scripts/View/Part.cs
@@ -16,23 +16,3 @@ namespace Cub.View
         }
     }
 }
-using System;
-using UnityEngine;
-
-namespace Cub.View
-{
-    public class Part : MonoBehaviour
-    {
-        public void Idle(float _After)
-        {
-            Invoke("_Idle", _After);
-            //Debug.Log("Bang!");
-        }
-
-        private void _Idle()
-        {
-            //Debug.Log("Bang!");
-            this.GetComponent<Animator>().SetTrigger("Idle");
-        }
-    }
-}
